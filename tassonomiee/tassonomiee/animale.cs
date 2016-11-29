ae675db29583ae0dc55b444142bd90c16c599ed0
unknown <cad@cad-18.itisriva.loc>
2016-11-29 10:11:19 +0100

    primo

---
+++ tassonomiee/tassonomiee/animale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tassonomiee
+{
+    abstract class animale
+    {
+        protected string nomeAnimale;
+        protected string tipoAnimale;
+        protected string habitat;
+        protected int numeroZampe;
+        public animale(string nome, string tipo, string abitat, int zampe)
+        {
+            nomeAnimale = nome;
+            tipoAnimale = tipo;
+            habitat = abitat;
+            numeroZampe = zampe;
+        }
+        public abstract string siMuove();
+        public void riepilogo()
+        {
+            Console.WriteLine("Nome:"+nomeAnimale);
+            Console.WriteLine("Tipo:"+tipoAnimale);
+            Console.WriteLine("Habitat:"+habitat);
+            Console.WriteLine("Numero di zampe:"+numeroZampe);
+        }
+    }
+}
